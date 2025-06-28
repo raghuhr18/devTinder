3ab37cd57988b0404a4585bf431c3273bf49396f
raghuhr18 <raghuhr18@yahoo.com>
2025-06-28 22:57:07 +0530

    installed Nodemon and started on routing

--- src/app.js
+++ src/app.js
@@ -2,9 +2,16 @@ const express = require("express")
 
 const app = express();
 
+app.use("/hello", (req, res) => {
+    res.send("Hello from Hello")
+})
+app.get("/user", (req, res) => {
+    res.send({"firstName": "John", "lastName": "Doe"})
+})
 app.use("/", (req,res) => {
     res.send("Hello from Dashboard")
 })
+
 app.listen(7777, () => {
     console.log("Listening.....!")
 })
