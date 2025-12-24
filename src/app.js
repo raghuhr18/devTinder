5893dcf00b3631162e4f625be22099a6081c33a9
raghuhr18 <raghuhr18@yahoo.com>
2025-12-24 15:46:14 +0530

    routing changes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const app = express();  
 
-  app.use("/test", (req, res) => {
-    res.send("Request received");
-})
   app.listen(7777, () => {
     console.log('Server is running on port 7777');
+  })
+  app.use("/test", (req, res) => {
+    res.send("Request received");
+  })
+  app.get("/user", (req, res) => {
+    res.send({firstName: "raghu", lastName: "Hegde"});
+  })
+  app.post("/user", (req, res) => {
+    res.send("Post request sent successfully");
+  })
+  app.put("/user",(req, res) => {
+    res.send("Put request sent successfully");
+  })
+  app.delete("/user",(req, res) => {
+    res.send("Delete request sent successfully");
+  })
+  app.use("/", (req, res) => {
+    res.send("Request received from root");
   })
--- src/homework.md
+++ src/homework.md
@@ -6,3 +6,15 @@ Listen to port 7777
 make request handlers..
 Install nodemon and update scrips inside package.json
 differences between carat and tilde
+
+
+04.
+- initialize git
+- create a remote repo on github
+- push code to remotre origin
+- check for the Order of routes, /hello, /test, /test/2/3
+- Install postman and test the Workspace/collection > testAPI call
+- Write logic to handle GET, POST, PATCH, DELETE, PUT Ai calls
+- Routing with use of ?, +, *, (), regex
+- Read the query parameters in the routes
+- Read the dynamic routes 
