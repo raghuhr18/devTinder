7cad8c341c0eee9d36a98d4093537880e88b9775
raghuhr18 <raghuhr18@yahoo.com>
2026-03-15 17:40:24 +0530

    tested the HTTP methods using Postman

--- src/app.js
+++ src/app.js
@@ -2,10 +2,23 @@ const express = require('express');
 
 const app = express();
 
+app.get('/user', (req, res) => {
+    res.send('This is from the get call');
+});
+
+app.post('/user', (req, res) => {
+    res.send('This is from the post call');
+});
+app.delete('/user', (req, res) => {
+    res.send('This is from the delete call');
+});
+app.put('/user', (req, res) => {
+    res.send('This is from the put call');
+});
+
 app.use("/test", (req, res) => {
     res.send('Hello, World! hello from test route');
 })
-
 app.listen(7778, () => {
     console.log('Server is running on port 7778');
 });
--- src/homework.md
+++ src/homework.md
@@ -11,3 +11,11 @@ What are dependencies
 what is the use of -g while installation
 Difference between caret and tilde
 
+Initialize git
+Create git ignore file, add .node-modules in it
+Set remote repoitory on github
+Push all code to remote origin
+Play with route extensions /hello, /hello/2, /test etc...
+Important: Order of routes matters a lot
+Install Postman create Workspace/Collection > test api call
+Write logic to handle /GET, /POST, /PATCH, DELETE API calls using Postman
